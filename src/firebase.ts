bd891c61f8dc0a8bbd36d5085b90daa852952b7c
kukwon <rnrdnjs415@daum.net>
2025-09-26 16:31:29 +0900

    firebase initialize

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsp7wbKZf3epm59dBXSfleGpid4A4u5x8",
+  authDomain: "nwitter-reloaded-f7c3f.firebaseapp.com",
+  projectId: "nwitter-reloaded-f7c3f",
+  storageBucket: "nwitter-reloaded-f7c3f.firebasestorage.app",
+  messagingSenderId: "987940761797",
+  appId: "1:987940761797:web:27fd8a27197f40c86b80ac",
+};
+
+const app = initializeApp(firebaseConfig);
